8a396ddeea4b9afb5c0b625d5a7f8ce8585bacd5
Nicole Banda <nmbanda124@gmail.com>
2024-12-18 14:10:31 -0800

    Added header and footer

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello</h1>
+        <h1>Definition Please!</h1>
       </header>
+      <footer className="App-footer">
+        <small>Coded by Nicole Banda</small>
+      </footer>
     </div>
   );
 }
