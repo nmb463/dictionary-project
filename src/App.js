8fa7a4bb0989b6973e058d5aa7985bd3e10f576d
Nicole Banda <nmbanda124@gmail.com>
2024-12-18 14:25:59 -0800

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Definition Please!</h1>
-      </header>
-      <footer className="App-footer">
-        <small>Coded by Nicole Banda</small>
-      </footer>
+      <div className="container">
+        <header className="App-header">
+          <h1>Definition Please!</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Nicole Banda</small>
+        </footer>
+      </div>
     </div>
   );
 }
